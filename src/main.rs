6974042aa54a7eff2c4998139cb46cf8b53bac3c
Afaan Bilal <afaanbilal@gmail.com>
2025-07-16 19:25:37 +0530

    🎉 Initial commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+//! # NanoCore
+//!
+//! (c) 2025 Afaan Bilal https://afaan.dev
+//!
+//! `NanoCore` is a meticulously crafted emulator for a custom, true 8-bit CPU.
+//!
+//! Designed with extreme minimalism in mind, this CPU operates within a strict
+//! 256-byte memory space, with all registers, the Program Counter (PC), and
+//! the Stack Pointer (SP) being 8-bit.
+//!
+//! This project serves as an educational exercise in understanding the
+//! fundamental principles of computer architecture, low-level instruction
+//! set design, memory management under severe constraints, and assembly
+//! language programming.
+//!
+
+fn main() {
+    println!("Hello, world!");
+}
